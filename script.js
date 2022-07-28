aee84f69a9013ae3e690623455b6f2346a680a99
akshatk806 <akshatk806@gmail.com>
2022-07-28 19:02:27 +0530

    Login form added

--- script.js
+++ script.js
@@ -7,5 +7,16 @@ searchBtn.addEventListener('click',()=>{
 });
 
 
+// Login modal
+let formBtn=document.querySelector('#login-btn');
+let loginForm=document.querySelector('.login-form-container');
+let formClose=document.querySelector('#form-close');
+
+formBtn.addEventListener('click',()=>{
+    loginForm.classList.add('active');
+});
+formClose.addEventListener('click',()=>{
+    loginForm.classList.remove('active');
+})
 
 
